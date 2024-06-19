cac3d5292cb4b89aa6f6c458919d01812fbf37fd
Thais Barbosa dos Santos <thaisbs1405@gmail.com>
2024-06-18 23:55:07 -0300

    corringindo páginas - pt2

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\Users\rosin\OneDrive\Favoritos compartilhados\Documentos\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\rosin\OneDrive\Favoritos compartilhados\Documentos\zoocare"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
